7198ad1575e8789b0de0a0a8617a36b669d81556
Leclassique <75534147+Leclassique@users.noreply.github.com>
2023-10-15 13:04:13 +0300

    Completed task

--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -5,11 +5,18 @@ import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        sourceList.sort(new ListComparator());
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int first = 5 * Integer.parseInt(a) * Integer.parseInt(a) + 3;
+        int second = 5 * Integer.parseInt(b) * Integer.parseInt(b) + 3;
+        if (first != second){
+            return Integer.compare(first, second);
+        } else {
+         return -1;
     }
-}
+}}
